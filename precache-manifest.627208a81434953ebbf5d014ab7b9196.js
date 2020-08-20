7c6aea490cc51cf62fbb38b4ed234037883d8d65
Mukesh salaria <mukesh.salaria01@gmail.com>
2020-08-20 13:29:36 +0530

    Updates

---
+++ precache-manifest.627208a81434953ebbf5d014ab7b9196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f63e9115c72177d07e0645833a87ff",
+    "url": "/reactDeploy/index.html"
+  },
+  {
+    "revision": "4b23912e8b4b40e36c1d",
+    "url": "/reactDeploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e0c1fabb70494e0c902",
+    "url": "/reactDeploy/static/js/2.eb9a376f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactDeploy/static/js/2.eb9a376f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b23912e8b4b40e36c1d",
+    "url": "/reactDeploy/static/js/main.a7c0c4b0.chunk.js"
+  },
+  {
+    "revision": "37cba6028fa690a3d205",
+    "url": "/reactDeploy/static/js/runtime-main.44903350.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
