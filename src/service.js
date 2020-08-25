488c15146683d74e473bf70961e24d85e5b5b947
Mukesh salaria <mukesh.salaria01@gmail.com>
2020-08-25 16:35:44 +0530

    VER-87

--- src/service.js
+++ src/service.js
@@ -1,34 +1,92 @@
+
 class ItemService {
-  constructor() {
-    this.items = [
-      {link:1, name:"test1", summary:"Summary Test 1", year:"2001", country:"us", price:"1000", description:"Desc 1"},
-      {link:2, name:"test2", summary:"Summary Test 2", year:"2002", country:"uk", price:"2000", description:"Desc 2"},
-      {link:3, name:"test3", summary:"Summary Test 3", year:"2003", country:"cz", price:"3000", description:"Desc 3"},
-    ];
-  }
-  async retrieveItems() {
-      return Promise.resolve(this.items);
-  }
-  async getItem(itemLink) {
-    for(var i = 0; i < this.items.length; i++) {
-      if ( this.items[i].link === itemLink) {
-        return Promise.resolve(this.items[i]);
-      }
+
+    async validateToken() {
+        return fetch('https://wakanda-cm.developerprogram.org/api/v1/validate', {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth-token": window.localStorage.token
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    this.handleResponseError(response);
+                }
+                return response.json();
+            })
+            .catch(error => {
+                this.handleError(error);
+            });
     }
-    return null;
-  }
-  async createItem(item) {
-    console.log("ItemService.createItem():");
-    console.log(item);
-    return Promise.resolve(item);
-  }
-  async deleteItem(itemId) {
-    console.log("ItemService.deleteItem():");
-    console.log("item ID:" + itemId);
-  }
-  async updateItem(item) {
-    console.log("ItemService.updateItem():");
-    console.log(item);
-  }
+
+    async refreshToken() {
+        return fetch('https://wakanda-cm.developerprogram.org/api/v1/accessToken', {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "refresh-token": window.localStorage.refreshToken
+            },
+        }).then(response => {
+            if (!response.ok) {
+                this.handleResponseError(response);
+            }
+            return response.json();
+        })
+            .catch(error => {
+                this.handleError(error);
+            });
+    }
+
+    async login(newitem) {
+        debugger
+        return fetch('https://wakanda-cm.developerprogram.org/api/v1/login', {
+            method: "post",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newitem)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    this.handleResponseError(response);
+                }
+                return response.json();
+            })
+            .catch(error => {
+                this.handleError(error);
+            });
+    }
+
+
+
+    async pageProperty() {
+        return fetch('https://wakanda-cm.developerprogram.org/api/v1/channel/page/show?uuid=7c41b988-ba6f-4785-9750-6075e18a3c1c', {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth-token": window.localStorage.token
+            },
+        }).then(response => {
+            if (!response.ok) {
+                this.handleResponseError(response);
+            }
+            return response.json();
+        })
+            .catch(error => {
+                this.handleError(error);
+            });
+    }
+
+    handleResponseError(response) {
+        throw new Error("HTTP error, status = " + response.status);
+    }
+    handleError(error) {
+        console.log(error);
+    }
 }
 export default ItemService;
--- src/test.js
+++ src/test.js
@@ -1,66 +1,232 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ItemService from './service';
-
-
+import { Button, Modal } from 'react-bootstrap';
 
 class Ribbon extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.itemService = new ItemService();   
-    this.onCreateItem = this.onCreateItem.bind(this);
 
-  }
-  render() {
+    constructor(props) {
+        super(props);
+
+
+        this.itemService = new ItemService();
+        this.setShow = this.setShow.bind(this);
+        this.chackToken = this.chackToken.bind(this);
+        this.onPageOpen = this.onPageOpen.bind(this);
+        this.login = this.login.bind(this);
+        this.refreshToken = this.refreshToken.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            showDetails: false,
+            showLogin: false,
+            showError: false,
+            open: false,
+            show: false,
+            searchable: false,
+            virtual: false
+        }
+    }
+
+    handleChange({ target }) {
+        this.setState({
+            [target.name]: target.value
+        });
+    }
+
+
+    render() {
+        const showDetails = this.state.showDetails;
+        const showLogin = this.state.showLogin;
+        const showError = this.state.showError;
+        const show = this.state.show;
+
         return (
-                 <>
-                    <div className="collapse" id="collapseExample">
-                        <div className="card bg-dark text-white rounded-0">
-                            <div className="container">
-                                <div className="card-body">
+            <>
+                <div className="collapse" id="collapseExample">
+                    <div className="card bg-dark text-white rounded-0">
+                        <div className="container">
+                            <div className="card-body">
 
-                                    <form id="login">
-                                        <div className="form-row">
-                                            <div className="col">
-                                                <input type="text" className="form-control" id="userName" placeholder="User Name" />
-                                            </div>
-                                            <div className="col">
-                                                <input type="password" className="form-control" id="password" placeholder="Password" />
-                                            </div>
-                                            <div className="col">
-                                                <button type="button" id="btnLogin1" className="btn btn-secondary">Log In</button>
-                                            </div>
+                                {showLogin && <form id="login" >
+                                    <div className="form-row">
+                                        <div className="col">
+                                            <input type="text" className="form-control" name="Username" placeholder="User Name" value={this.state.username}
+                                                onChange={this.handleChange} />
                                         </div>
-                                        <span id="logInError"></span>
-                                    </form>
-
-                                    <div className="dropdown hidden" id="dropdown">
-                                        <button className="btn btn-secondary" type="button" id="pagePropertyModel">
-                                            Page Property
-							</button>
-
+                                        <div className="col">
+                                            <input type="password" className="form-control" name="Password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                                        </div>
+                                        <div className="col">
+                                            <button type="button" id="btnLogin1" onClick={this.login} className="btn btn-secondary">Log In</button>
+                                        </div>
                                     </div>
+                                    {showError && <span id="logInError"> Invalid username & password.</span>}
 
-                                </div>
+                                </form>}
+
+                                {showDetails && <div className="dropdown"><button className="btn btn-secondary" type="button" onClick={this.onPageOpen}>Page Property</button></div>}
+
                             </div>
                         </div>
                     </div>
-                    <button onClick={this.onCreateItem} className="btn bg-dark text-white float-right collapse-btn" type="button" id="collapse-button" data-toggle="collapse"
-                        data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        <span >+</span>
-                    </button>
-                 </>
-            
+                </div>
+                <button onClick={this.chackToken} className="btn bg-dark text-white float-right collapse-btn" type="button" id="collapse-button" data-toggle="collapse"
+                    data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <span >+</span>
+                </button>
+
+
+                <Modal
+                    show={show}
+                    onHide={this.setShow}
+                    dialogClassName="modal-90w"
+                    aria-labelledby="example-custom-modal-styling-title">
+                    <Modal.Header closeButton>
+                        <Modal.Title id="example-custom-modal-styling-title">
+                            Page Properties
+                    </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body style={{ marginLeft: '20%' }}>
+
+                        <div className="form-group row">
+                            <div className="col-md-6 row">Page Name</div>
+                            <div className="col-md-6">
+                                <input type="text" className="from-control" readOnly value={this.state.PageName} ></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="col-md-6 row">Page Display Name</div>
+                            <div className="col-md-6">
+                                <input type="text" className="from-control" readOnly id="txtPageDisplayName" value={this.state.PageDisplayName} ></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="col-md-6 row">Discription</div>
+                            <div className="col-md-6">
+                                <input type="text" className="from-control" readOnly id="txtDiscription" value={this.state.Discription} ></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="col-md-6 row">Searchable</div>
+                            <div className="col-md-6">
+                                <input type="checkbox" className="from-control" id="chkSearchable" defaultChecked={this.state.searchable} onChange={this.handleChangeChk}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="col-md-6 row">Virtual</div>
+                            <div className="col-md-6">
+                                <input className="from-control" type="checkbox" id="chkVirtual" defaultChecked={this.state.virtual} onChange={this.handleChangeChk12}></input>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+            </>
         );
-  }
-    onCreateItem(newItem) {   
-    this.itemService.createItem(newItem).then(item => {
-        
-      }
-    );
-  }
-  
+    }
+
+
+
+    login() {
+        var data = {
+            username: this.state.Username,
+            password: this.state.Password
+        };
+        this.itemService.login(data).then(item => {
+            if (item != undefined && item.username != null) {
+                window.localStorage.refreshToken = item.refreshToken;
+                window.localStorage.token = item.accessToken;
+                this.setState({
+                    showDetails: true,
+                    showLogin: false
+                });
+            }
+            else {
+                this.setState({
+                    showError: true,
+                    showDetails: false,
+                    showLogin: true
+                });
+            }
+        });
+    }
+
+    chackToken() {
+        this.itemService.validateToken().then(item => {
+            if (item != undefined && item.username != null) {
+                window.localStorage.refreshToken = item.refreshToken;
+                window.localStorage.token = item.accessToken;
+                this.setState({
+                    showDetails: true,
+                    showLogin: false
+                });
+            }
+            else {
+                this.setState({
+                    showDetails: false,
+                    showLogin: true
+                });
+            }
+        });
+    }
+
+    refreshToken() {
+        this.itemService.refreshToken().then(item => {
+            if (item != undefined && item.username != null) {
+                window.localStorage.refreshToken = item.refreshToken;
+                window.localStorage.token = item.accessToken;
+                this.setState({
+                    showDetails: true,
+                    showLogin: false
+                });
+            } else {
+                this.setState({
+                    showDetails: false,
+                    showLogin: true
+                });
+            }
+        });
+    }
+
+    onPageOpen() {
+        this.itemService.pageProperty().then(item => {
+            if (item) {
+
+                if (item.data.searchable) {
+                    this.setState({
+                        searchable: true
+                    });
+                }
+
+
+                if (item.data.virtual) {
+                    this.setState({
+                        virtual: true
+                    });
+                }
+
+                this.setState({
+                    show: true,
+                    PageName: item.data.nodeName,
+                    PageDisplayName: item.data.displayName,
+                    Discription: item.data.description,
+                    //uuid: item.data.UUID
+                });
+            } else {
+                this.setState({
+                    showDetails: false,
+                    showLogin: true
+                });
+            }
+        });
+    }
+
+    setShow() {
+        this.setState({
+            show: !this.state.show
+        });
+    }
+
+
 }
 
 export default Ribbon;
