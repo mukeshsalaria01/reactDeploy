f3915cf21717933cc590e8dd2958f2fc100d6194
Mukesh salaria <mukesh.salaria01@gmail.com>
2020-08-26 17:04:49 +0530

    VER-87

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import Ribbon from './test';
 import * as serviceWorker from './serviceWorker';
 
 
-
-  // <React.StrictMode>
-    // <App />
-  // </React.StrictMode>,
-  //document.getElementById('root')
-     //ReactDOM.render(<App />, document.getElementById("root"));
-
-
 ReactDOM.render(<Ribbon />, document.getElementById('root123'));
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
